e6e95cf24697092a22c5578588b6edb3408faae3
fntp <stickpoint@163.com>
2023-06-29 18:07:48 +0800

    DDMusic 0036-新增歌单详情页面 （1）顶点音乐新增歌单详情页面

---
+++ src/main/java/com/stickpoint/ddmusic/page/controller/PlayListDetailController.java
@@ -0,0 +1,15 @@
+package com.stickpoint.ddmusic.page.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+/**
+ * @author puye(0303)
+ * @since 2023/6/29
+ */
+public class PlayListDetailController {
+
+
+    @FXML
+    public VBox playListRootPane;
+}
